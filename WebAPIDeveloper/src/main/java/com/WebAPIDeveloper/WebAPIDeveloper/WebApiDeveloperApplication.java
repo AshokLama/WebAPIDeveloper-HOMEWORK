9b6a23ba200192c87282d43e18fc48f8454903bf
Ashok Lama <kosha.amal@gmail.com>
2023-09-26 18:24:52 -0600

    Add files via upload
    "first commit "

---
+++ WebAPIDeveloper/src/main/java/com/WebAPIDeveloper/WebAPIDeveloper/WebApiDeveloperApplication.java
@@ -0,0 +1,13 @@
+package com.WebAPIDeveloper.WebAPIDeveloper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApiDeveloperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApiDeveloperApplication.class, args);
+	}
+
+}
---
+++ WebAPIDeveloper/src/test/java/com/WebAPIDeveloper/WebAPIDeveloper/WebApiDeveloperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WebAPIDeveloper.WebAPIDeveloper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApiDeveloperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
